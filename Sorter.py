8381e8b1f07a20fad53355504d27e088d512196e
DAVID  SMYTH <13383861@IT.NUIGALWAY.IE>
2017-09-07 19:03:34 +0100

    initial commit

---
+++ Sorter.py
@@ -0,0 +1,22 @@
+#main sorter class
+import sys
+import ast
+class Sorter:
+    def __init__(self):
+        pass
+
+    def load_sorting_method(self, sorting_method_name):
+        pass
+
+    def sort_python(self,list_to_sort, method, element_type = None):
+        pass
+
+    def sort_cpp(self,list_to_sort, method, element_type = None):
+        pass
+    
+    def get_available_sorting_methods(self):
+        pass
+
+    def profile_sorting_methods(self):
+        pass
+        
