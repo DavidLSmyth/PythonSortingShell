3a81d87ee15db71be711a727f05f9dfef405be31
DavidLSmyth <d.smyth10@nuigalway.ie>
2017-09-09 00:36:46 +0100

    added tests

--- SortingClassesTest.py
+++ SortingClassesTest.py
@@ -6,5 +6,33 @@ Created on Fri Sep  8 20:02:05 2017
 """
 
 import unittest
-class SortingTest(unittest.Unittest):
-    pass
+import random
+
+from SortingBase import * 
+from SortingClasses import *
+
+class SortingTest(unittest.TestCase):
+    
+    def test_mergesort(self):
+        x = MergeSort(False)
+        l = [random.randint(-1000,1000) for i in range(50)]
+        self.assertEqual(sorted(l), x.sort(l))
+      
+    def test_bubblesort(self):
+        x = BubbleSort(False)
+        l = [random.randint(-1000,1000) for i in range(50)]
+        self.assertEqual(sorted(l), x.sort(l))
+        
+    def test_bubblesort_recursive(self):
+        x = BubbleSortRecursive(False)
+        l = [random.randint(-1000,1000) for i in range(50)]
+        self.assertEqual(sorted(l), x.sort(l))
+        
+        
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
