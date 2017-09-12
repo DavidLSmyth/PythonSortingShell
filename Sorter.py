cdcc2e1a81f536f1d86883006e87b80615081056
DavidLSmyth <d.smyth10@nuigalway.ie>
2017-09-12 11:56:11 +0100

    Added experimental load_all_methods unload_all_methods functionality

--- Sorter.py
+++ Sorter.py
@@ -1,4 +1,4 @@
-#base imports
+#stdlib imports
 import sys
 import ast
 import inspect
@@ -13,20 +13,44 @@ import matplotlib.pyplot as plt
 #user defined modules
 import SortingBase
 
-#from SortingClasses import *
+
 class Sorter:
     def __init__(self):
         self._loaded_sorting_methods = []
+        
+    def get_loaded_methods(self):
+        return(list(map(lambda x: x.__name__, self._loaded_sorting_methods)))
 
     def load_sorting_method(self, sorting_method_name):
         '''loads class name into current workspace'''
         #check whether name has already been loaded
-        print('global {}'.format(sorting_method_name),'from SortingClasses import {}'.format(sorting_method_name,sorting_method_name ))
-        exec('global {}'.format(sorting_method_name))
-        exec('from SortingClasses import {}'.format(sorting_method_name,sorting_method_name ))
-        print('Loaded {} into workspace'.format(sorting_method_name))
-        self._loaded_sorting_methods.append(eval(sorting_method_name))
-
+        if sorting_method_name not in list(map(lambda x: x.__name__,self._loaded_sorting_methods)):
+            try:
+                exec('global {}'.format(sorting_method_name))
+                exec('from SortingClasses import {}'.format(sorting_method_name,sorting_method_name ))
+                print('Loaded {} into current workspace'.format(sorting_method_name))
+                self._loaded_sorting_methods.append(eval(sorting_method_name))
+            except ImportError as e:
+                print('Could not load {} into the workspace'.format(sorting_method_name))
+        else:
+            print('{} has already been loaded into the workspace'.format(sorting_method_name))
+        
+    def load_all_methods(self):
+        for sorting_method in self._get_available_sorting_methods():
+            self.load_sorting_method(sorting_method.__name__)
+        
+    def unload_sorting_method(self, sorting_method_name):
+        '''unloads a single method from the workspace'''
+        if sorting_method_name in self._loaded_sorting_methods:
+            del sorting_method_name
+            self._loaded_sorting_method.remove(sorting_method_name)
+            print('unloaded {} from the workspace'.format(sorting_method_name))
+        else:
+            print('{} was not loaded: cannot remove'.format(sorting_method_name))
+            
+    def unload_all_loaded_method(self):
+        '''unloads all loaded sorting methods from the workspace'''
+        map(self.unload_sorting_method, list(map(lambda x: x.__name__,self._loaded_sorting_methods)))
 
     def sort_python(self,iterable: collections.Iterable, sorting_method):
         if(sorting_method in self._loaded_sorting_methods):
@@ -37,7 +61,7 @@ class Sorter:
     def sort_cpp(self,list_to_sort, method, element_type = None):
         pass
     
-    def get_available_sorting_methods(self) -> 'dict {sorting_method_repr: class}':
+    def _get_available_sorting_methods(self) -> 'dict {sorting_method_repr: class}':
         return[sorting_class for sorting_class in SortingBase.SortingBase.__subclasses__()]
 
     def _get_average_runtime(self,sorting_method,iterable_type = int, input_size = 50):
@@ -62,24 +86,24 @@ class Sorter:
         test_sizes = [int((100*i)) for i in range(20,60,4)]
         sorting_method_runtimes = {}
         if all_methods:
-            for sorting_class in self.get_available_sorting_methods():
-                print('profiling: {}'.format(sorting_class.__name__))
-                self.load_sorting_method(sorting_class.__name__)
-                sorting_method_runtimes[sorting_class.__name__] = list(map(lambda x: self._get_average_runtime(sorting_class(True), input_size = x), test_sizes))
+            #make a copy of currently loaded methods
+            currently_loaded_methods = self._loaded_sorting_methods
+            self.load_all_methods()
                 
-        else:
-            for sorting_class in self._loaded_sorting_methods:
-                print('profiling: {}'.format(sorting_class.__name__))
-                sorting_method_runtimes[sorting_class.__name__] = list(map(lambda x: self._get_average_runtime(sorting_class(True), input_size = x), test_sizes))
+        for sorting_class in self._loaded_sorting_methods:
+            print('profiling: {}'.format(sorting_class.__name__))
+            sorting_method_runtimes[sorting_class.__name__] = list(map(lambda x: self._get_average_runtime(sorting_class(True), input_size = x), test_sizes))
+        
         return (test_sizes,sorting_method_runtimes)
         
 
 x = Sorter()
 x.load_sorting_method('QuickSort')
 x.load_sorting_method('MergeSort')
+x.load_all_methods()
 #x.load_sorting_method('BubbleSort')
 x.load_sorting_method('InsertionSort')
-print(x.get_available_sorting_methods())
+print(x._get_available_sorting_methods())
 #list(x.get_available_sorting_methods().values())[0]().sort(['a','b','c','z','f'])
 times = x.profile_sorting_methods(all_methods=False)
 xs = times[0]
