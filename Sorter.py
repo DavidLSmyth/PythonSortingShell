d392720415ede22e7b5fbced187ff4eb9f69f864
DavidLSmyth <d.smyth10@nuigalway.ie>
2017-09-12 12:45:42 +0100

    Ready to write tests for sorter.py

--- Sorter.py
+++ Sorter.py
@@ -15,11 +15,18 @@ import SortingBase
 
 
 class Sorter:
+    '''A class which can load and unload sorting classes from SortingClasses into the workspace. 
+    Methods exist which can then benchmark loaded sorting classes against each other'''
     def __init__(self):
         self._loaded_sorting_methods = []
         
     def get_loaded_methods(self):
+        '''Returns the names of the sorting methods that have been loaded into the workspace'''
         return(list(map(lambda x: x.__name__, self._loaded_sorting_methods)))
+    
+    def get_sorting_class(self,sorting_method_name):
+        '''returns the sorting class given it's name'''
+        return list(filter(lambda x: x.__name__ == sorting_method_name, self._loaded_sorting_methods))[0]
 
     def load_sorting_method(self, sorting_method_name):
         '''loads class name into current workspace'''
@@ -41,9 +48,10 @@ class Sorter:
         
     def unload_sorting_method(self, sorting_method_name):
         '''unloads a single method from the workspace'''
-        if sorting_method_name in self._loaded_sorting_methods:
-            del sorting_method_name
-            self._loaded_sorting_method.remove(sorting_method_name)
+        if sorting_method_name in self.get_loaded_methods():
+            self._loaded_sorting_methods.remove(self.get_sorting_class(sorting_method_name))
+            #del list(filter(lambda x: x.__name__ == sorting_method_name, self._loaded_sorting_methods))[0]
+            #don't need to remove this from the list as it's already been removed from the workspace
             print('unloaded {} from the workspace'.format(sorting_method_name))
         else:
             print('{} was not loaded: cannot remove'.format(sorting_method_name))
@@ -82,8 +90,9 @@ class Sorter:
     def profile_sorting_methods(self, all_methods = False):
         '''Runs specified sorting methods on different sized inputs and offers a summary 
         of the runtimes of each.'''
+        print('This may take a while...')
         #maybe use yield here because it takes a while!
-        test_sizes = [int((100*i)) for i in range(20,60,4)]
+        test_sizes = [int((100*i)) for i in range(20,40,4)]
         sorting_method_runtimes = {}
         if all_methods:
             #make a copy of currently loaded methods
@@ -94,6 +103,8 @@ class Sorter:
             print('profiling: {}'.format(sorting_class.__name__))
             sorting_method_runtimes[sorting_class.__name__] = list(map(lambda x: self._get_average_runtime(sorting_class(True), input_size = x), test_sizes))
         
+        for method_to_unload in list(filter(lambda x: x not in currently_loaded_methods, self._loaded_sorting_methods)):
+            self.unload_sorting_method(method_to_unload.__name__)
         return (test_sizes,sorting_method_runtimes)
         
 
@@ -101,21 +112,26 @@ x = Sorter()
 x.load_sorting_method('QuickSort')
 x.load_sorting_method('MergeSort')
 x.load_all_methods()
+x.unload_sorting_method('QuickSort')
+x.get_loaded_methods()
+x._loaded_sorting_methods
+x.profile_sorting_methods()
+
 #x.load_sorting_method('BubbleSort')
 x.load_sorting_method('InsertionSort')
-print(x._get_available_sorting_methods())
-#list(x.get_available_sorting_methods().values())[0]().sort(['a','b','c','z','f'])
-times = x.profile_sorting_methods(all_methods=False)
-xs = times[0]
-ys = list(times[1].values())
-labels = list(times[1].keys())
-for y,label_name in zip(ys, labels):
-    print(y,label_name)
-    plt.plot(xs, y,'p-',label = label_name)
-#plt.plot(xs, ys[0],'p-', label = labels[0])
-#plt.plot(xs, ys[1],'p-',label = labels[1])
-#plt.plot(xs, ys[2],'p-',label = labels[2])
-plt.legend(loc = 'upper left')
-plt.ylabel('Time in milliseconds to run')
-plt.xlabel('Size of input')
+#print(x._get_available_sorting_methods())
+##list(x.get_available_sorting_methods().values())[0]().sort(['a','b','c','z','f'])
+#times = x.profile_sorting_methods(all_methods=False)
+#xs = times[0]
+#ys = list(times[1].values())
+#labels = list(times[1].keys())
+#for y,label_name in zip(ys, labels):
+#    print(y,label_name)
+#    plt.plot(xs, y,'p-',label = label_name)
+##plt.plot(xs, ys[0],'p-', label = labels[0])
+##plt.plot(xs, ys[1],'p-',label = labels[1])
+##plt.plot(xs, ys[2],'p-',label = labels[2])
+#plt.legend(loc = 'upper left')
+#plt.ylabel('Time in milliseconds to run')
+#plt.xlabel('Size of input')
 
