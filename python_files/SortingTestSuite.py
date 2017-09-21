d5a4c1fd8abcdeac7c724be6fd2ab43ed678250b
DavidLSmyth <d.smyth10@nuigalway.ie>
2017-09-21 18:17:19 +0100

    updated result of travis test to reflect build

--- python_files/SortingTestSuite.py
+++ python_files/SortingTestSuite.py
@@ -7,14 +7,18 @@ Created on Tue Sep 12 19:44:34 2017
 
 #test suite 
 
-
+#stdlib
 import unittest
+import sys
+
+#user defined
 from SorterTest import TestSorter
 from SortingClassesTest import TestSortingClasses
 
 test_sorter = unittest.TestLoader().loadTestsFromTestCase(TestSorter)
 test_sorting_classes = unittest.TestLoader().loadTestsFromTestCase(TestSortingClasses)
-testsuite = unittest.TestSuite([test_sorter, test_sorting_classes]) 
-unittest.TextTestRunner(verbosity=2).run(testsuite)
+test_suite = unittest.TestSuite([test_sorter, test_sorting_classes]) 
+runner_result = unittest.TextTestRunner(verbosity=2).run(test_suite).wasSuccessful()
 #.run(testsuite)
-
+#ret = not runner.run(test_suite).wasSuccessful()
+sys.exit(not runner_result)
